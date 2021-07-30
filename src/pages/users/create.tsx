c411c6620b19bf7e354e4d6911317fdb54f764e7
Alexandre <alexandrevoliveira30@gmail.com>
2021-07-30 12:34:50 -0300

    add: Form de criacao de Usuarios em /users/create

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,138 @@
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
+
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+
+interface CreateUserFormData {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required("Nome obrigatório"),
+  email: yup.string().required("Email obrigatório").email("E-mail inválido"),
+  password: yup.string().required("Senha obrigatória")
+    .min(6, "Senha deve ter no mínimo 6 caracteres"),
+  password_confirmation: yup.string().oneOf([
+    null, yup.ref('password')
+  ], "As senhas não batem")
+})
+
+export default function CreateUser() {
+  const router = useRouter()
+
+  const createUser = useMutation(async(user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  })
+
+  const { register, formState, handleSubmit } = useForm({
+    resolver: yupResolver(createUserFormSchema)
+  });
+
+  const { errors } = formState;
+
+  const handleCreateUser:SubmitHandler<CreateUserFormData> = async(values) => {
+    await createUser.mutateAsync(values)
+
+    router.push('/users')
+  };
+
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8} 
+          p={["6","8"]}
+          bg="gray.800"
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
+          <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
+
+          <Divider borderColor="gray.700" my="6"/>
+
+          <VStack>
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input
+                name="name"
+                type="text"
+                label="Nome completo"
+                error={errors.name}
+                {...register('name')}
+              />
+              <Input
+                name="email"
+                type="email"
+                label="E-mail"
+                error={errors.email}
+                {...register('email')}
+              />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input
+                name="password"
+                type="password"
+                label="Senha"
+                error={errors.password}
+                {...register('password')}
+              />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmação de senha"
+                error={errors.password_confirmation}
+                {...register('password_confirmation')}
+              />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
+
+              <Button
+                type="submit"
+                colorScheme="pink"
+                isLoading={formState.isSubmitting}
+              >
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
